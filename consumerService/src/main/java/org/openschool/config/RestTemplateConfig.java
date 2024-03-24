0e74600494eb23790a12870cee736b9c06879386
Nikolas <9225407@gmail.com>
2024-03-24 07:23:12 +0300

    eureka-server

--- consumerService/src/main/java/org/openschool/config/RestTemplateConfig.java
+++ consumerService/src/main/java/org/openschool/config/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package org.openschool.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import java.time.Duration;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
                 .setConnectTimeout(Duration.ofSeconds(5))
